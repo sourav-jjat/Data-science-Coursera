0b71cb0e25869c23c6edf07d3183b545245edb05
Sourav Ijjat <49354914+sourav-jjat@users.noreply.github.com>
2020-08-18 00:40:53 -0500

    Checking the procvess of commiting on 08-18-2020

--- First script.R
+++ First script.R
@@ -1,3 +1,4 @@
 print("File created with Rstudio")
 print("file should be on github")
 print("Testing if anything happens if local directory on computer is changed")
+print("Restesting what was learnt")
