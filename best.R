ca2674f9f357a05171fb2a0725cfdbd79034f21e
Sourav Ijjat <49354914+sourav-jjat@users.noreply.github.com>
2020-10-26 09:28:19 -0500

    code for week 4. Part 2. sETTING WORKING DIRECTORY

---
+++ best.R
@@ -0,0 +1,12 @@
+best <- function(state = character(), state = character()){
+  # set working directory to the folder
+  setwd("C:/Users/soura/Documents/Data_Science_Spec_Coursera/Data-science-Coursera/Week 4")
+  
+  
+  
+  
+  
+  
+  
+  
+}
