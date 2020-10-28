7ba46617804fe363301f8195cff51dae3265592d
Sourav Ijjat <49354914+sourav-jjat@users.noreply.github.com>
2020-10-28 01:06:06 -0500

    made a more efficient code to collect hospital names by further dividing the dataframe into 3 columns

--- rankall.R
+++ rankall.R
@@ -23,6 +23,15 @@ rankall <- function(diaganosis = character(), num){
   names(df)[4] <- "heart failure"
   names(df)[5] <- "pneumonia"
   
+  #further reducing the dataframe to suit our needs
+  if (diagnosis == "heart attack") {
+    df <- df[,c(1, 2, 3)]
+  }else if (diagnosis == "heart failure") {
+    df <- df[,c(1, 2, 4)]
+    
+  }else{df <- df[,c(1, 2, 5)]}
+  
+
   #sorting by diagnosis and then sorting by hospital name
   df <- df[order(df[,colnames(df) == diagnosis],df$Hospital.Name),]
   
@@ -32,26 +41,25 @@ rankall <- function(diaganosis = character(), num){
   #splitting as per states
   best_state <- split.data.frame(df, df$State)
   
-  for (variable in states) {
+  for (variable in states[1:length(states)]) {
     #dummy variable to store a state information in one
     best_in_state <- best_state[[variable]]
     
+    best_in_state_complete <- best_in_state[complete.cases(best_in_state),]
     ##checking if the rank exceeds number of hospitals in state
     
     if (num > nrow(best_in_state)) {
-      hospital_name_num <- append(hospital_name_num,NA)
-    }
-    if (is.null(best_in_state$diagnosis[num]) == TRUE) {
-      hospital_name_num <- append(hospital_name_num, NA)
-    }
-    if (num == "best") {
-      hospital_name_num <- append(hospital_name_num,best_in_state$Hospital.Name[1])
+      hospital_name_num <- append(hospital_name_num,as.character(NA))
+    }else if (num == "best") {
+      hospital_name_num <- append(hospital_name_num,best_in_state_complete$Hospital.Name[1])
     }else if (num == "worst") {
-      hospital_name_num <- append(hospital_name_num, best_state$Hospital.Name[nrow(best_state) - sum(is.na(best_state[,colnames(best_state) == diagnosis]))])
-    } else {
-      hospital_name_num <- append(hospital_name_num, best_state$Hospital.Name[num])
-    }
     
+      hospital_name_num <- append(hospital_name_num, best_in_state_completes$Hospital.Name[nrow(best_in_state_complete)])
+    } else {
+      
+      hospital_name_num <- append(hospital_name_num, best_in_state_complete$Hospital.Name[num])
+    }
+    #variable <- NULL
   }
   ranks <- data.frame(Hospital = hospital_name_num, State = states)
 }
